edc0bc55a046ec1c19491f302f982d31ac55c851
André Ekeberg <hello@andreekeberg.se>
2023-02-05 22:26:24 +0100

    Add Prediction to exports to allow for type checks

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import Classifier from './Classifier'
 
 export { default as Model } from './Model'
 export { default as Vocabulary } from './Vocabulary'
+export { default as Prediction } from './Prediction'
 export { Classifier as Classifier }
 
 export default Classifier
